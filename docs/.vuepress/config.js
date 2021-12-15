7a52c8fb7bc2aaf4660a374ca275257d615ec3d3
liuzeqian <1099795081@qq.com>
2021-12-15 18:40:25 +0800

    push

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,22 @@
 // .vuepress/config.js
 module.exports = {
+    title: '欣农组件库',
     base: '/xinnongUI/',
     themeConfig: {
+        nav: [{
+                text: 'Home',
+                link: '/',
+            },
+            {
+                text: 'Github',
+                link: 'https://github.com/liuzeqian/xinnongUI',
+            },
+            //https://www.npmjs.com/package/xinnong-ui
+            {
+                text: 'npm',
+                link: 'https://www.npmjs.com/package/xinnong-ui',
+            },
+        ],
         sidebar: ['/', '/componentsDocs/card'],
     },
 }
